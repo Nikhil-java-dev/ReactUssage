581d8aedae2c074591ba0d40663e8028555779fe
Nikhil Srivastava <srivastavanikhil756@gmail.com>
2026-02-23 14:43:33 +0530

    React-router-dom part-1 start

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,16 @@
-import Parent from "./Topic-17 useCallback/Parent";
-
-
-
-
-
-
+import About from "./Topic-21 React Router Part-1/About";
+import Header from "./Topic-21 React Router Part-1/Header";
+import Home from "./Topic-21 React Router Part-1/Home";
+import "./Topic-21 React Router Part-1/style.css";
+import {Outlet} from "react-router-dom"
 
 const App = () => {
   return (
-     <div id="app-component">
-            <Parent />
-      </div>
-  );
-};
+    <div id="app-component">
+      <Header/>
+      <Outlet/>      
+    </div>
+  )
+}
 
-export default App;
-// This is a simple React application that imports and uses two components: Header and Footer.
-// The App component serves as the main component of the application, rendering the Header at the top and the Footer at the bottom.
-// 
-// To run this application, you would typically use a tool like Vite or Create React App to set up your development environment. Once set up, you can start the development server and view your application in a web browser.
-// 
-// This structure allows for easy maintenance and scalability, as you can add more components or features in the future without affecting the overall layout.
-// 
-// To use this code, ensure you have the necessary dependencies installed (like React and ReactDOM) and that your project is set up to handle JSX syntax. You can then import this App component into your main entry file (like index.js or main.jsx) and render it to the DOM.
+export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,47 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import { Children, StrictMode } from "react";
+import {createRoot} from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
-  // console.log('App', App()),
+
+import App from "./App.jsx";
+import Home from "./Topic-21 React Router Part-1/Home.jsx";
+import About from "./Topic-21 React Router Part-1/About.jsx";
+import Contact from "./Topic-21 React Router Part-1/Contact.jsx";
+import NotFound from "./Topic-21 React Router Part-1/NotFound.jsx";
+
+/**
+ * Router Configuration Setup
+ */
+
+const appRouter = createBrowserRouter([
+  {
+    path:"/",
+    element:<App/>,
+    children:[
+      {
+        path:"",
+        element:<Home/>
+      },
+      {
+        path:"about",
+        element: <About/>
+      },
+      {
+        path:"contact",
+        element:<Contact/>
+      }
+    ],
+    errorElement:<NotFound/>
+  }
+  
+])
+
+
+
+
+
+createRoot(document.getElementById("root")).render(
+  <RouterProvider  router={appRouter} />
+ );
  
-    <App />
-
-)
+  
